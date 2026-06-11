fe23609a197cca380b3769b4a500d7554dbc95fd
AnJungHoon <jh96.dev@gmail.com>
2026-06-11 15:24:11 +0900

    refactor: 폴더 구조 정리

--- review/복습_5장_260609.R
+++ review/복습_5장_260609.R
@@ -97,11 +97,16 @@ head(DF, 5)
 colnames(DF) <- c("age", "salary", "specialSalary", "workingTime", "numberOfWorker", "career", "sex")
 str(DF)
 
+Scale <- scale(DF$salary)
+head(Scale, 10)
 
+DF <- cbind(DF, scale=Scale)
+str(DF)
 
-
-
-
+g1 <- ggplot(DF, aes(x=scale, y=age))
+g2 <- geom_segment(aes(yend=age), xend=0)
+g3 <- g1 + g2 + geom_point(size=7, aes(colour=sex, shape=career)) + theme_minimal()
+g3
 
 
 
